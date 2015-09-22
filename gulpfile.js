ec52e8bdafc66d9e7ef0abc69d36966020c79db8
Johann Musyznski <johann.muszynski@gmail.com>
2015-09-22 16:22:13 +0300

    remove old cruft

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var concat = require('gulp-concat');
 
 var config = {
     // include all js files, but exclude any min.js files
-    src: [ 'libs/**/*.js', 'src/**/*.js', '!src/**/*min.js' ]
+    src: [ 'libs/**/*.js', 'src/graphics.js', 'src/file_io.js', 'src/mesh_tools.js', 'src/trackball.js', 'src/geometry.js', 'src/morphoviewer.js' ]
 }
 
 gulp.task( 'release', function() {
